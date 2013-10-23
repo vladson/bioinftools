80aa7f4a52a0b989c587725da0c38eae11e421c0
Vladislav Bogomolov <vladson4ik@gmail.com>
2013-10-24 00:53:25 +0400

    Doctests for Dna

--- dna_tools/dna.py
+++ dna_tools/dna.py
@@ -8,10 +8,20 @@ class Dna:
         self.conversion_table = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
 
     def complementary(self):
+        """
+        >>> dna = Dna('AAAACCCGGT')
+        >>> dna.complementary()
+        'ACCGGGTTTT'
+        """
         return ''.join(map(lambda c: self.conversion_table[c], self.genome))[::-1]
 
 
     def substr_finder(self, length = 3):
+        """
+        >>> dna = Dna("ACGTTGCATGTCGCATGATGCATGAGAGCT")
+        >>> dna.substr_finder(4)
+        'CATG GCAT'
+        """
         results = {}
         for i in range(0, len(self.genome) - length):
             substr = self.genome[i:i + length]
@@ -22,8 +32,20 @@ class Dna:
         maximum = max(results.values())
         return ' '.join(map(lambda (key,_): key, filter(lambda (_,v): v == maximum, results.iteritems())))
 
-if __name__ == '__main__':
-    dna = Dna("ACGTTGCATGTCGCATGATGCATGAGAGCT")
-    print(dna.substr_finder(4))
-    raw_input()
+    def starting_positions(self, fragment):
+        """
+        >>> dna = Dna("GATATATGCATATACTT")
+        >>> dna.starting_positions('ATAT')
+        [1, 3, 9]
+        """
+        positions = []
+        fragment_length = len(fragment)
+        for i in range(0, len(self.genome) - fragment_length):
+           if self.genome[i:fragment_length + i] == fragment:
+               positions.append(i)
+        return positions
+
+    def h_starting_positions(self, fragment):
+        
+
 
